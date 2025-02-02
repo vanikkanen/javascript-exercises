998a9484bb075903ea9f969235be158c1db53bfe
vanikkanen <vanikkanen@gmail.com>
2025-02-02 11:54:35 +0200

    Complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    if (num1 < 0 ||
+        num2 < 0 ||
+        !Number.isInteger(num1) ||
+        !Number.isInteger(num2)) 
+        return "ERROR"
+
+
+    let small = num1 < num2 ? num1 : num2
+    let large = num1 > num2 ? num1 : num2
+
+    let sum = 0
+
+    for (let i = small; i <= large; i++) {
+        sum += i
+    }
+    return sum
 
 };
 
