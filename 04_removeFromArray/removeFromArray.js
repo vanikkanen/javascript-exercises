20711a5603659dd5ecd151a34132ec6674979da1
vanikkanen <vanikkanen@gmail.com>
2025-02-02 11:42:03 +0200

    Complete fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+    const ansArr = []
+    for (let i in arr) {
+        let num = arr[i]
+        if (!args.includes(num)) {
+            ansArr.push(num)
+        }
+    }
+    return ansArr
 };
 
 // Do not edit below this line
