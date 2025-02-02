8f67f3288be60a439ebad069047291b77b0e2b9d
vanikkanen <vanikkanen@gmail.com>
2025-02-02 11:28:00 +0200

    Complete third exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    if (num < 0) return "ERROR"
+    let returnString = ""
 
+    for (let i = 0; i < num; i++) {
+        returnString += string
+    }
+
+    return returnString
 };
 
 // Do not edit below this line
