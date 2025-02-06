507d2019448b82f289f4d1c9833f89442587bcea
Valtteri Nikkanen <valtteri.nikkanen@valmet.com>
2025-02-06 09:45:57 +0200

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let stripped = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase().split('')
+    let i = 0, j = stripped.length-1
+    while (i <= j) {
+        if (stripped[i] !== stripped[j]) return false
+        i++
+        j--
+    }
+    return true
 };
 
 // Do not edit below this line
