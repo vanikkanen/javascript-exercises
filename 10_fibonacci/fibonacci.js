cfd76973cb0d3addd3891bead60174061117b156
Valtteri Nikkanen <valtteri.nikkanen@valmet.com>
2025-02-06 09:58:19 +0200

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+
+    index = +index
+    if (index < 0) return "OOPS"
+
+    let fibSeq = [0,1,1]
+
+    if (index < fibSeq.length) {
+        return fibSeq[index]
+    }
+    let prevPrev = 1
+    let prev = 1
+    let ans
+    for (let i = 3; i <= index; i++) {
+        ans = prev+prevPrev
+        prevPrev = prev
+        prev = ans
+    }
+    return ans
 
 };
 
