449ac91cf4083df2dce14f058def5f3da6579d4e
Valtteri Nikkanen <valtteri.nikkanen@valmet.com>
2025-02-06 09:36:01 +0200

    Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,47 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, val) => acc + val, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, val) => acc*val, 1)
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+
+  if (power === 0) return 1
+
+  let ans = 1
+  for(let i = 0; i < power; i++) {
+    ans *= num
+  }
+  return ans
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+
+  let ans = 1
+  while (num > 0) {
+    ans *= num
+    num--
+  }
+  return ans
+
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
