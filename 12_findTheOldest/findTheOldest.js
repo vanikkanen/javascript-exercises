e3c7abb6542ede564ccc9a2db016d3d5adabb4c0
Valtteri Nikkanen <valtteri.nikkanen@valmet.com>
2025-02-06 11:00:46 +0200

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+
+    // return peopleArr.sort((a, b) => {
+    //     let aAge = (a.yearOfDeath ? a.yearOfDeath : (new Date()).getFullYear()) - a.yearOfBirth
+    //     let bAge = (b.yearOfDeath ? b.yearOfDeath : (new Date()).getFullYear()) - b.yearOfBirth
+    //     return bAge - aAge
+    // })[0]
+
+    return peopleArr.reduce((oldest, person) => {
+        let personAge = (person.yearOfDeath ? person.yearOfDeath : (new Date()).getFullYear()) - person.yearOfBirth
+        let oldestAge = (oldest.yearOfDeath ? oldest.yearOfDeath : (new Date()).getFullYear()) - oldest.yearOfBirth
+        return personAge > oldestAge ? person : oldest
+    }, peopleArr[0])  
 
 };
 
